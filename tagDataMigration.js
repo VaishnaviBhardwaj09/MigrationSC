ddd0a655fb3da119ede31500a95c28f0ef6ab23e
INNOBIT <innobit@localhost.localdomain>
2023-01-30 09:46:05 +0530

    Change in the Script

--- tagDataMigration.js
+++ tagDataMigration.js
@@ -5,27 +5,19 @@ console.log("Started the Script>>>>>>>>>>>>>>>>>>")
 
 // old prod string url
 const uri = 'mongodb://diUser:6A200sj9ZlHjuXZHtIo2@sc-prod-new-di-database.cluster-ckpzwf1jxpvt.eu-west-1.docdb.amazonaws.com:27017/digital-identity?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false'
-mongoose.createConnection(uri, { tlsCAFile: `rds-combined-ca-bundle.pem` });
-var oldDB = mongoose.connection
+var oldDB = mongoose.createConnection(uri, { tlsCAFile: `rds-combined-ca-bundle.pem` });
+
 oldDB.on('error', console.error.bind(console, 'connection error:'));
 
 
 // new prod string url
 var qaDB = mongoose.createConnection('mongodb://solution-dev-qamongo:HbylJkfMu8lwRwlAHOWqID9SY256BEnBO9Ulj0awumaJ6VETOOr6cAXu2Od6WQjg5QwOQEzI7ZerACDbyvkF0w==@solution-dev-qamongo.mongo.cosmos.azure.com:10255/smartcosmos_qa?ssl=true&retryWrites=true');
 qaDB.on('error', console.error.bind(console, 'connection error:'));
-// qaDB('open', async function () {
-//     qaDB.db.listCollections().toArray(function (err, collections) {
-//         console.log(collections);
-//     });
-// })
+
 
 
 oldDB.once('open', async function () {
     console.log("Connection Successful!");
-    // oldDB.db.listCollections().toArray(function (err, collections) {
-    //     console.log(collections);
-    // });
-
     // manufacture data object
     let dataObj = {
         userId: "",
@@ -77,7 +69,7 @@ oldDB.once('open', async function () {
     let distinctBatchId = await oldDB.collection("CustomerBatch").distinct("batchId")
     console.log("DistinctBatchId======================>", distinctBatchId)
     for (let batch of distinctBatchId) {
-        let tagsData = await oldDB.collection("Tag").find({bId:batch.batchId}).toArray();
+        let tagsData = await oldDB.collection("Tag").find({bId:batch}).toArray();
         for (let tag of tagsData) {
             try {
                 console.log("Tag collection ===> tag Id: ", tag._id);
@@ -86,45 +78,38 @@ oldDB.once('open', async function () {
                 dataObj.secureKey = tag.seq;   // doubt
                 dataObj.tagType = tag.ttl;    // doubt
                 batchDataObj.batchId = tag.bId;
-                data.historyReferenceId = uuid.v4();
+                dataObj.historyReferenceId = uuid.v4();
                 batchDataObj.historyReferenceId = uuid.v4();
 
 
                 let customerBatchData = await oldDB.collection("CustomerBatch").findOne({ 'batchId': tag.bId });
-
-                data.orderId = customerBatchData.orderId;
-                data.orderDate = customerBatchData.orderDate;
-                data.orderQuantity = customerBatchData.orderQuantity;
-                data.orderQuantityUnit = customerBatchData.orderQuantityUnit;
-                data.deliveryDate = customerBatchData.deliveryDate;
-                data.deliveryItemName = customerBatchData.deliveryItemName;
-                data.deliveryQuantity = customerBatchData.deliveryQuantity;
-                data.deliveryQuantityUnit = customerBatchData.deliveryQuantityUnit;
-                data.inlayItemName = customerBatchData.batchProperties.inlayItemName;
-                data.inlayType = customerBatchData.batchProperties.inlayType;
-
-
-                let customerData = await oldDB.collection("Customer").findOne({ 'customerId': customerBatchData.customerId });
-                data.customerName = customerData.customerName;
-                data.manufacturerName = customerData.customerName;
-
-
+                if(customerBatchData!=null){
+                    dataObj.orderId = customerBatchData.orderId  ;
+                    dataObj.orderDate = customerBatchData.orderDate ;
+                    dataObj.orderQuantity = customerBatchData.orderQuantity ;
+                    dataObj.orderQuantityUnit = customerBatchData.orderQuantityUnit;
+                    dataObj.deliveryDate = customerBatchData.deliveryDate;
+                    dataObj.deliveryItemName = customerBatchData.deliveryItemName;
+                    dataObj.deliveryQuantity = customerBatchData.deliveryQuantity;
+                    dataObj.deliveryQuantityUnit = customerBatchData.deliveryQuantityUnit;
+                    dataObj.inlayItemName = customerBatchData.batchProperties.inlayItemName;
+                    dataObj.inlayType = customerBatchData.batchProperties.inlayType;
+                    let customerData = await oldDB.collection("Customer").findOne({ 'customerId': customerBatchData.customerId });
+                    dataObj.customerName = customerData.customerName;
+                    dataObj.manufacturerName = customerData.customerName;
+                }
                 // new production mongo
                 let prodManufacturerTagsRes = await qaDB.collection("manufacturerTags").findOne({ 'diId': { $eq: tag._id } });
 
                 if (prodManufacturerTagsRes) {
                     dataObj.status = 'Inactive';
                     dataObj.isActivated = false;
-
+                    delete dataObj._id
                     let respIn = await qaDB.collection("duplicateManufactureTags").insertOne(dataObj);
-
                     console.log("Tag exist in manufacturer tags =====> duplicateManufactureTags==================>  ");
-
                     continue;
                 }
-
                 let prodDigitizedTagsRes = await qaDB.collection("digitizedTag").findOne({ 'diId': { $eq: tag._id } });
-
                 if (prodDigitizedTagsRes) {
                     dataObj.tenantId = prodDigitizedTagsRes.tenantId;
                     dataObj.tenantName = prodDigitizedTagsRes.tenantName;
@@ -134,7 +119,8 @@ oldDB.once('open', async function () {
                     dataObj.status = 'active';
                     dataObj.isActivated = true;
 
-
+                    delete dataObj._id
+                    delete batchDataObj._id
                     await qaDB.collection("manufacturerTags").insertOne(dataObj);
 
                     await qaDB.collection("batch").insertOne(batchDataObj);
@@ -144,7 +130,10 @@ oldDB.once('open', async function () {
 
                 }
                 else {
+                    delete dataObj._id
+                    delete batchDataObj._id
                     await qaDB.collection("unassignedTags").insertOne(dataObj);
+                    await qaDB.collection("unassignedBatch").insertOne(batchDataObj)
 
                     console.log("Tag not exist in digitized tags =====> unassignedTags ==================>  ");
                 }
